80d2597c66781273c32d484483c5f2d753e9221d
Aichurok Choloeva <aichualym@gmail.com>
2026-05-04 12:58:12 +0200

    Initial Commit

---
+++ KontoKlar/KontoKlarApp.swift
@@ -0,0 +1,17 @@
+//
+//  KontoKlarApp.swift
+//  KontoKlar
+//
+//  Created by Aichurok Choleva on 04.05.26.
+//
+
+import SwiftUI
+
+@main
+struct KontoKlarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
